18359b7072630a5adcbb1ecee7bb68bcecf2543a
Mark Verleg <mark@localhost>
2022-09-14 22:12:00 +0200

    Write help text

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -30,11 +30,34 @@ fn parse_args(args: Vec<String>) -> Result<Option<String>, String> {
                 .ok_or_else(|| format!("argument -s/--source expects a single argument containing source code"))?
                 .to_owned()))
         },
-        Some(arg) => Err(format!("unknown argument '{arg}'; try --help for options")),
+        Some(arg) => {
+            let hint = if arg.contains("=") {
+                "hint: --arg=value syntax is not supported, use '--arg value'\n"
+            } else {
+                ""
+            };
+            Err(format!("unknown argument '{arg}'\n{hint}try --help for options"))
+        },
         None => Err(format!("expected at least one argument; try --help for options")),
     }
 }
 
 fn gen_help() -> String {
-    "help here".to_owned()
+    vec![
+        format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")),
+        format!("{}", env!("CARGO_PKG_DESCRIPTION")),
+        format!("more info: {} ; by: {}", env!("CARGO_PKG_HOMEPAGE"), env!("CARGO_PKG_AUTHORS")),
+        "".to_owned(),
+        "USAGE:".to_owned(),
+        format!("    {} -s 'source here'", env!("CARGO_PKG_NAME")),
+        format!("    {} --file ./source.~", env!("CARGO_PKG_NAME")),
+        format!("    cat input.txt | {} -s 'source here'", env!("CARGO_PKG_NAME")),
+        //TODO @mverleg: put better example source ^
+        "".to_owned(),
+        "OPTIONS:".to_owned(),
+        "    -h, --help        Show this help".to_owned(),
+        "    -s, --source S    Run source S, which should be golfed source with unicode encoding".to_owned(),
+        "    -f, --file P      Run source contained in file at path P, which should be golfed".to_owned(),
+        "                      source with unicode encoding".to_owned(),
+    ].join("\n")
 }
