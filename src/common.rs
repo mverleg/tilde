b21f714c937fc708aa20e42e0b56fad9b6451349
Mark Verleg <mark@localhost>
2022-09-15 21:12:50 +0200

    Use atomic u8 to toggle if logging is on

--- src/common.rs
+++ src/common.rs
@@ -1,16 +1,28 @@
-use ::std::cell::OnceCell;
+use ::std::sync::atomic::AtomicU8;
+use ::std::sync::atomic::Ordering;
+use ::std::env;
 
-static LOG_ON: OnceCell<bool> = OnceCell::new();
+const UNKNOWN: u8 = 1;
+const ON: u8 = 1;
+const OFF: u8 = 2;
+static LOG_ON: AtomicU8 = AtomicU8::new(0);
 
 macro_rules! log {
     ($templ:literal, $($args:expr)*) => {
         {
-            let is_on = LOG_ON.get_or_init(|| {
-                let is_on = env::var("TILDE_LOG");
-                !(matches!(None, is_on) || matches!(Some(""), is_on) || matches!(Some("0"), is_on))
-            });
-            if is_on {
-                eprintln!($templ);
+            // let is_on = LOG_ON.store(|| {
+            //     let is_on = env::var("TILDE_LOG");
+            //     !(matches!(None, is_on) || matches!(Some(""), is_on) || matches!(Some("0"), is_on))
+            // }, Ordering::Release);
+            let mut is_on = LOG_ON.load(Ordering::Acquire);
+            if is_on == UNKNOWN {
+                let on_env = env::var("TILDE_LOG");
+                let is_off = matches!(None, on_env) || matches!(Some(""), on_env) || matches!(Some("0"), on_env);
+                is_on = if is_off { OFF } else { ON };
+                LOG_ON.store(is_on, Ordering::Release);
+            }
+            if is_on == ON {
+                eprintln!($templ, $($args, )*);
             }
         }
     }
@@ -18,7 +30,6 @@ macro_rules! log {
 
 #[cfg(test)]
 mod tests {
-    use std::env;
     use super::*;
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![feature(once_cell)]
-
 use ::std::env;
 use ::std::process::ExitCode;
 
