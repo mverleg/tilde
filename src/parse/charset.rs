7a1cd352c285538a446e3fb89fd174b1d69d5e94
Mark Verleg <mark@localhost>
2022-09-16 21:35:41 +0200

    Unit test for Charset

--- src/parse/charset.rs
+++ src/parse/charset.rs
@@ -9,23 +9,72 @@ pub enum TokenType {
     Modifier,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Token {
+#[derive(Debug, Clone)]
+pub struct Token {
     byte: u8,
     chr: char,
     long: String,
+    typ: TokenType,
 }
 
 impl Token {
-    pub const fn new(byte: u8, chr: char, long: impl Into<String>) -> Self {
+    pub const fn new(byte: u8, chr: char, long: impl Into<String>, typ: TokenType) -> Self {
         Token {
             byte,
             chr,
-            long,
+            long: long.into(),
+            typ
         }
+    }
+
+    pub const fn var(byte: u8, chr: char, long: impl Into<String>) -> Self {
+        Token::new(byte, chr, long, TokenType::VariableOpen)
+    }
+
+    pub const fn fixed(byte: u8, chr: char, long: impl Into<String>) -> Self {
+        Token::new(byte, chr, long, TokenType::FixedOpen)
+    }
+
+    pub const fn modi(byte: u8, chr: char, long: impl Into<String>) -> Self {
+        Token::new(byte, chr, long, TokenType::Modifier)
     }
 }
 
-const CHARSET: [Token; u8::MAX as usize] = [
-    Token::new(b'a', 'a', "a"),
+pub const CHARSET: [Token; 1] = [
+    Token::fixed(b'a', 'a', "a"),
 ];
+
+#[cfg(test)]
+mod tests {
+    use ::std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn unique_bytes() {
+        let mut seen = HashSet::new();
+        for c in CHARSET {
+            assert!(seen.insert(c.byte))
+        }
+        assert_eq!(seen.len(), u8::MAX)
+    }
+
+    #[test]
+    fn unique_char() {
+        let mut seen = HashSet::new();
+        for c in CHARSET {
+            assert!(seen.insert(c.chr))
+        }
+        assert_eq!(seen.len(), u8::MAX)
+    }
+
+    #[test]
+    fn unique_long_identifier() {
+        let mut seen = HashSet::new();
+        for c in CHARSET {
+            assert!(seen.insert(c.long))
+        }
+        assert_eq!(seen.len(), u8::MAX)
+    }
+}
+
