e1eacd29bcbaa78c0be2f24c3ebea80c7ceafa69
Mark <mverleg.noreply@gmail.com>
2022-09-23 22:00:31 +0200

    Only show non-modifiers in token list

--- doc/README.md
+++ doc/README.md
@@ -1,5 +1,5 @@
 
 # Tilde reference (v0.1.0)
 
-All 256 openers: 0 | i | n | ^ | " | + | x | = | > | $ | : | [ | ! | ? | # | ~ | 
+Openers: [0](./num.md) | [i](./inp.md) | [n](./seq.md) | [.](./more.md) | ["](./str.md) | [+](./plus.md) | [x](./x.md) | [=](./eq.md) | [>](./gt.md) | [$](./var.md) | [:](./forall.md)
 
--- src/gen/mddoc.rs
+++ src/gen/mddoc.rs
@@ -1,15 +1,24 @@
 use ::std::fs;
 
 use crate::gen::doc::gen_grouped_docs;
+use crate::parse::TOKENSET;
 use crate::TildeRes;
 
 pub fn gen_md_docs() -> TildeRes<()> {
     let docs = gen_grouped_docs();
     let mut docbuf = format!("\n# Tilde reference (v{})\n\n", env!("CARGO_PKG_VERSION"));
     let mut tlreadme = core::fmt::Formatter::new(&mut docbuf);
-    write!(tlreadme, "All 256 openers: ").unwrap();
+    write!(tlreadme, "Openers: ").unwrap();
+    let mut is_first = true;
     for (opener, groups) in docs {
-        write!(tlreadme, "{} | ", opener.chr).unwrap();
+        if ! opener.is_modifier() {
+            if is_first {
+                is_first = false;
+            } else {
+                write!(tlreadme, " | ").unwrap();
+            }
+            write!(tlreadme, "[{}](./{}.md)", opener.chr, &opener.long).unwrap();
+        }
     }
     write!(tlreadme, "\n\n").unwrap();
     fs::create_dir_all("doc")
