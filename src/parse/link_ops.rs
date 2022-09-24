68ae77daf27cfe73fd834b179b3c4b7406594064
Mark <mverleg.noreply@gmail.com>
2022-09-24 09:28:36 +0200

    Start code for building ast, but assume linear

--- src/parse/link_ops.rs
+++ src/parse/link_ops.rs
@@ -1,5 +1,54 @@
+use crate::ast::{Op, Prog};
 use crate::parse::TokenGroup;
+use crate::TildeRes;
 
-pub fn link_ops(tokens: &[TokenGroup]) {
-    todo!()
+pub fn link_ops(tokens: &[TokenGroup]) -> TildeRes<Prog> {
+    let mut ops = vec![];
+    let mut missing = vec![];
+    for token_group in tokens {
+        match link_op(token_group) {
+            Some(op) => ops.push(op),
+            None => missing.push(token_group),
+        }
+    }
+    tokens.into_iter()
+        .map(|tokens| link_op(tokens).ok_or_else(|| ))
+        .collect()
+}
+
+pub fn link_op(group: &TokenGroup) -> Option<Op> {
+    todo!();
+}
+
+fn parse(source: &str) -> TildeRes<Prog> {
+    let mut ops = vec![];
+    for c in source.chars() {
+        ops.push(match c {
+            '!' => Op::Math1(Math1Op::Neg),
+            '‖' => Op::Math1(Math1Op::Abs),
+            '↑' => Op::Math1(Math1Op::Incr),
+            '↓' => Op::Math1(Math1Op::Decr),
+            '+' => Op::Math2(Math2Op::Plus),
+            '-' => Op::Math2(Math2Op::Minus),
+            '*' => Op::Math2(Math2Op::Mul),
+            '/' => Op::Math2(Math2Op::Div),
+            '\\' => Op::Math2(Math2Op::IntDiv),
+            '%' => Op::Math2(Math2Op::Mod),
+            '=' => Op::Compare(CompareOp::Eq),
+            '≠' => Op::Compare(CompareOp::Neq),
+            '>' => Op::Compare(CompareOp::Gt),
+            '≥' => Op::Compare(CompareOp::Gte),
+            '<' => Op::Compare(CompareOp::Lt),
+            '≤' => Op::Compare(CompareOp::Lte),
+            '&' => Op::Bool2(Bool2Op::And),
+            '|' => Op::Bool2(Bool2Op::Or),
+            //'a' => Op::Bool2(Bool2Op::Nand),  //TODO @mverleg:
+            '⊕' => Op::Bool2(Bool2Op::Xor),
+            '→' => Op::Bool2(Bool2Op::Impl),
+            '0'..='9' => Op::Value(ValueOp::Number(c.to_digit(10).unwrap() as f64)),
+            '←' => unimplemented!(),
+            sym => Err(format!("unknown source symbol: {sym}"))?,
+        })
+    }
+    Ok(Prog::of(ops))
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -14,36 +14,3 @@ mod tokengroup;
 mod tokenset;
 mod tokenize;
 mod link_ops;
-
-pub fn parse(source: &str) -> TildeRes<Prog> {
-    let mut ops = vec![];
-    for c in source.chars() {
-        ops.push(match c {
-            '!' => Op::Math1(Math1Op::Neg),
-            '‖' => Op::Math1(Math1Op::Abs),
-            '↑' => Op::Math1(Math1Op::Incr),
-            '↓' => Op::Math1(Math1Op::Decr),
-            '+' => Op::Math2(Math2Op::Plus),
-            '-' => Op::Math2(Math2Op::Minus),
-            '*' => Op::Math2(Math2Op::Mul),
-            '/' => Op::Math2(Math2Op::Div),
-            '\\' => Op::Math2(Math2Op::IntDiv),
-            '%' => Op::Math2(Math2Op::Mod),
-            '=' => Op::Compare(CompareOp::Eq),
-            '≠' => Op::Compare(CompareOp::Neq),
-            '>' => Op::Compare(CompareOp::Gt),
-            '≥' => Op::Compare(CompareOp::Gte),
-            '<' => Op::Compare(CompareOp::Lt),
-            '≤' => Op::Compare(CompareOp::Lte),
-            '&' => Op::Bool2(Bool2Op::And),
-            '|' => Op::Bool2(Bool2Op::Or),
-            //'a' => Op::Bool2(Bool2Op::Nand),  //TODO @mverleg:
-            '⊕' => Op::Bool2(Bool2Op::Xor),
-            '→' => Op::Bool2(Bool2Op::Impl),
-            '0'..='9' => Op::Value(ValueOp::Number(c.to_digit(10).unwrap() as f64)),
-            '←' => unimplemented!(),
-            sym => Err(format!("unknown source symbol: {sym}"))?,
-        })
-    }
-    Ok(Prog::of(ops))
-}
