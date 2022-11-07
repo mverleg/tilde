a228fc594b020dd3a9644a650bbe0f43d68369da
Mark <mverleg.noreply@gmail.com>
2022-11-07 23:38:35 +0100

    Allow single-quotes for long-mode strings

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -18,16 +18,16 @@ pub fn parse(src: &str) -> TildeRes<Prog> {
     tilde_log!("parsing {} tokens", tokens.len());
     let mut buffer = String::new();
     while let Some(current) = tokens.pop() {
-        if current == ',' {
+        if current == ',' || current == '\'' {
             buffer.clear();
             while let Some(token) = tokens.pop() {
-                if token == ',' {
+                if token == ',' || token == '\'' {
                     //TODO @mark: build a way to escape commas
                     break;
                 }
                 buffer.push(token)
             }
-            tilde_log!("string literal (long mode): \"{}\"", &buffer);
+            tilde_log!("string literal (long mode): '{}'", &buffer);
             let op = Op::Value(ValueOp::Text(buffer.clone()));
             ops.push(op)
         } else if (current >= '1' && current <= '9') || current == '.' || current == '-' {
@@ -43,6 +43,19 @@ pub fn parse(src: &str) -> TildeRes<Prog> {
             }
             tilde_log!("integer literal (long mode): \"{}\"", &buffer);
             let op = Op::Value(ValueOp::Number(buffer.parse::<f64>().map_err(|err| format!("invalid number '{}', err {}", buffer, err))?));
+            ops.push(op)
+        } else if current.is_alphabetic() {
+            buffer.clear();
+            buffer.push(current);
+            while let Some(token) = tokens.pop() {
+                if !current.is_alphabetic() {
+                    tokens.push(token);
+                    break;
+                }
+                buffer.push(token)
+            }
+            tilde_log!("operator by long name: \"{}\"", &buffer);
+            let op = todo!("{}", current);
             ops.push(op)
         } else if current == '"' {
             tilde_log!("string lookup (short mode)");
@@ -64,14 +77,16 @@ mod tests {
 
     #[test]
     fn long_string_explicit_close() {
-        let prog = parse(",hello world,").unwrap();
-        assert_eq!(prog, of(Op::Value(ValueOp::Text("hello world".to_string()))))
+        assert_eq!(parse(",hello world,").unwrap(), of(Op::Value(ValueOp::Text("hello world".to_string()))));
+        assert_eq!(parse(",hello world'").unwrap(), of(Op::Value(ValueOp::Text("hello world".to_string()))));
+        assert_eq!(parse("'hello world,").unwrap(), of(Op::Value(ValueOp::Text("hello world".to_string()))));
+        assert_eq!(parse("'hello world'").unwrap(), of(Op::Value(ValueOp::Text("hello world".to_string()))));
     }
 
     #[test]
     fn long_string_implicit_close() {
-        let prog = parse(",hello world").unwrap();
-        assert_eq!(prog, of(Op::Value(ValueOp::Text("hello world".to_string()))))
+        assert_eq!(parse(",hello world").unwrap(), of(Op::Value(ValueOp::Text("hello world".to_string()))));
+        assert_eq!(parse("'hello world").unwrap(), of(Op::Value(ValueOp::Text("hello world".to_string()))));
     }
 
     #[test]
