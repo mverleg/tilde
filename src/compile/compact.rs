1652fe91a879b2a5ba4400d37e0e81fc573066dd
Mark <mverleg.noreply@gmail.com>
2022-11-09 15:30:36 +0100

    Hard-code allowed openers and keep them in sync

--- src/compile/compact.rs
+++ src/compile/compact.rs
@@ -1,14 +1,26 @@
+use ::strum::IntoEnumIterator;
+
+use crate::compile::letter::Letter;
 use crate::op::Op;
+
+const STRING_OPENERS: [Letter; 10] = [Letter::Number, Letter::Io, Letter::Seq, Letter::More, Letter::Plus, Letter::Asterisk, Letter::Slash, Letter::Right, Letter::Bracket, Letter::Colon];
 
 /// Encode a positive integer, using static width of 1 byte each, and
 /// do not allow modifiers in the first byte.
-pub fn encode_positive_int_static_width_avoid_modifiers(nr: u64) -> Vec<Op> {
+pub fn encode_positive_int_static_width_avoid_modifiers(nr: u64) -> Vec<Letter> {
     todo!();
 }
 
 /// Inverse of [encode_pos_int_static_width_avoid_modifiers].
-pub fn decode_positive_int_static_width_avoid_modifiers(ops: &[Op]) -> Option<u64> {
+pub fn decode_positive_int_static_width_avoid_modifiers(ops: &[Letter]) -> Option<u64> {
     if ops.is_empty() {
+        return None;
+    }
+    let opener = &ops[0];
+    if let Letter::Text = opener {
+        return None;
+    }
+    if Letter::modifiers().contains(opener) {
         return None;
     }
     todo!();
@@ -26,10 +38,24 @@ pub fn decode_positive_int_static_width_avoid_modifiers(ops: &[Op]) -> Option<u6
 #[cfg(test)]
 mod static_width {
     use super::*;
+    use crate::compile::letter::LetterKind;
+
+    //TODO @mark: specific example cases
+
+    #[test]
+    fn string_openers_in_sync() {
+        let allowed_openers: Vec<Letter> = Letter::iter().filter(|letter| letter != &Letter::Text).filter(|letter| letter.kind() != LetterKind::Modifier).collect();
+        assert_eq!(allowed_openers, &STRING_OPENERS);
+        assert!(STRING_OPENERS.len() >= 1);
+    }
 
     #[test]
     fn positive_int_without_avoided_modifiers() {
-        todo!();
+        for nr in [0, 1, 100, 100_000] {
+            let enc = encode_positive_int_static_width_avoid_modifiers(nr);
+            let dec = decode_positive_int_static_width_avoid_modifiers(&enc).unwrap_or_else(|| panic!("failed to decode {}", nr));
+        }
+        todo!()
     }
 
     #[test]
