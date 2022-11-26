f5a05ecc92fd9851a9b492c395391f130fb2e484
Mark <mverleg.noreply@gmail.com>
2022-11-27 00:40:45 +0100

    Encode empty uint vecs

--- src/compile/txt_literal.rs
+++ src/compile/txt_literal.rs
@@ -9,10 +9,42 @@ use crate::op::Op;
 use crate::NR;
 use crate::UINT;
 
-fn encode_uint_vec(nrs: &[UINT]) -> Vec<Letter> {
-    unimplemented!() //TODO @mark: TEMPORARY! REMOVE THIS!
+#[derive(Debug, Copy, Clone)]
+pub enum Closer {
+    Text,
+    Number,
 }
 
-pub fn decode_uint_vec(letters: &[Letter]) -> Vec<UINT> {
+///
+/// Note: the first letter cannot be a modifier or number token, so if the length is zero
+/// and the closer is supposed to be number, we have too pick text token instead.
+pub fn encode_uint_vec(
+    nrs: &[UINT],
+    closer: Closer,
+) -> Vec<Letter> {
+    if nrs.is_empty() {
+        return vec![Text];
+    }
     unimplemented!()
+}
+
+pub fn decode_uint_vec(letters: &[Letter]) -> (Vec<UINT>, Closer) {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encode_empty_nr() {
+        let enc = encode_uint_vec(&[], Closer::Number);
+        assert_eq!(enc, vec![Text]);
+    }
+
+    #[test]
+    fn encode_empty_txt() {
+        let enc = encode_uint_vec(&[], Closer::Text);
+        assert_eq!(enc, vec![Text]);
+    }
 }
