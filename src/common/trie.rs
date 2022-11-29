3dd6be0148c45e98f0b785be57533abdeaca29e6
Mark <mverleg.noreply@gmail.com>
2022-11-29 22:42:45 +0100

    Get rid of optimizations, just use hashmaps in trie

--- src/common/trie.rs
+++ src/common/trie.rs
@@ -1,28 +1,9 @@
 use ::std::collections::HashMap;
 
 #[derive(Debug)]
-enum TrieChildren {
-    /// If there are only a few children, store them on the stack, with the empty ones at the end marked as dummy.
-    Few([TrieNode; 3]),
-    /// There are more than a few children, so we want to allocate a map for faster lookup
-    Many(HashMap<char, TrieNode>),
-}
-
-#[derive(Debug, Default)]
-enum TrieNodeType {
-    /// Not an actual node, just a dummy in [TrieChildren::Few]
-    #[default]
-    Dummy,
-    /// A node that is a word, and may also have children
-    Word,
-    /// A node that has children, but is not itself a word
-    Link,
-}
-
-#[derive(Debug)]
 struct TrieNode {
-    typ: TrieNodeType,
-    children: TrieChildren,
+    children: HashMap<char, TrieNode>,
+    is_word: bool,
 }
 
 #[derive(Debug)]
@@ -32,7 +13,7 @@ pub struct Trie {
 
 impl Trie {
     pub fn new() -> Self {
-        Trie { root: TrieNode { children: TrieChildren::empty(), typ: TrieNodeType::Dummy } }
+        Trie { root: TrieNode { children: HashMap::with_capacity(0), is_word: false } }
     }
 }
 
