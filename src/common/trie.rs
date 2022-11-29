66dbaa2ec1d1ac712bcb2ed9ac8a4c20c9227133
Mark <mverleg.noreply@gmail.com>
2022-11-29 22:46:13 +0100

    Add push test, util methods for trie

--- src/common/trie.rs
+++ src/common/trie.rs
@@ -6,6 +6,15 @@ struct TrieNode {
     is_word: bool,
 }
 
+impl TrieNode {
+    pub fn new_empty() -> Self {
+        TrieNode {
+            children: HashMap::with_capacity(0),
+            is_word: false
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Trie {
     root: TrieNode
@@ -13,7 +22,13 @@ pub struct Trie {
 
 impl Trie {
     pub fn new() -> Self {
-        Trie { root: TrieNode { children: HashMap::with_capacity(0), is_word: false } }
+        Trie {
+            root:TrieNode::new_empty()
+        }
+    }
+
+    pub fn push(&mut self, value: &str) {
+        todo!()
     }
 }
 
@@ -24,5 +39,15 @@ mod tests {
     #[test]
     fn empty() {
         let trie = Trie::new();
+        //TODO @mark: lookup
+    }
+
+    #[test]
+    fn build() {
+        let mut trie = Trie::new();
+        trie.push("hello");
+        trie.push("he");
+        trie.push("hell");
+        trie.push("hey");
     }
 }
