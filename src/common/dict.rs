ab5957ff5f7169b6a0c957a9100d215db734e5fd
Mark <mverleg.noreply@gmail.com>
2022-11-29 19:15:12 +0100

    Implement lookup for dictionary, but problem with one-time-init

--- src/common/dict.rs
+++ src/common/dict.rs
@@ -3,16 +3,42 @@
 //TODO @mark: 0 is reserved (for backspace)
 //TODO @mark: fallback to full unicode after end?
 
-static RAW_DICT: &'static str = include_str!("../../dictionary.txt");
-static DICT: Dictionary = Dictionary::new();
+use ::std::sync::{Arc, Mutex, RwLock};
 
+static RAW_DICT: &'static str = include_str!("../../dictionary.txt");
+static DICT: Arc<RwLock<DictContainer>> = Arc::new(RwLock::new(DictContainer::new()));
+
+struct DictContainer {
+    snippet_lookup: Vec<&'static str>,
+}
+
+#[derive(Clone)]
 pub struct Dictionary {
-    dict: Vec<&'static str>,
+    container: Arc<RwLock<DictContainer>>,
+}
+
+impl DictContainer {
+    const fn new() -> Self {
+        DictContainer {
+            snippet_lookup: vec![],
+        }
+    }
 }
 
 impl Dictionary {
-    const fn new() -> Self {
-        unimplemented!()
+    fn new() -> Self {
+        Dictionary {
+            container: DICT.clone(),
+        }
+    }
+
+    fn index(&self, position: usize) -> Option<&'static str> {
+        assert!(position != 0, "cannot look up position 0, it is reserved (dict starts at 1)");
+        let read_container = self.container.read().expect("RwLock poisoned");
+        if ! read_container.snippet_lookup.is_empty() {
+            return read_container.snippet_lookup.get(position).map(|txt| *txt)
+        }
+        todo!()
     }
 }
 
@@ -22,6 +48,7 @@ mod lookup {
 
     #[test]
     fn test_whitespace() {
-        assert!(DICT[0] == " ", "first entry should be space");
+        let dict = Dictionary::new();
+        assert_eq!(dict.index(0), Some(" "), "first entry should be space");
     }
 }
