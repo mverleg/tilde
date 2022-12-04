33f3250ed6e1d4556e616a5a6e8e4a4e8e44c014
Mark <mverleg.noreply@gmail.com>
2022-12-05 00:07:08 +0100

    Trying to generate file in output dir, but cnanot find it

--- build.rs
+++ build.rs
@@ -11,17 +11,21 @@ fn main() {
 fn derived_dict_entries() {
     println!("cargo:rerun-if-changed=src/common/dict_derive.rs");
     println!("cargo:rerun-if-changed=dictionary.txt");
+    let out_file = concat!(env!("OUT_DIR"), "/dictionary_extended.txt");
+    println!("cargo:rerun-if-changed={out_file}");
+    println!("cargo:rerun-if-changed=dictionary.txt");
     let base_dict = fs::read_to_string("./dictionary.txt").unwrap();
-    let original = base_dict.lines().collect::<Vec<_>>();
+    let original = base_dict.lines()
+        .filter(|s| !s.is_empty() && s.chars().filter(|c| *c == '$').count() < 2)
+        .collect::<Vec<_>>();
     let derived = collect_derivations(&original);
     println!("would count: {} -> {}", original.len(), derived.len());
     let dict_str = sorted_join(derived);
-    fs::write("dictionary_extended.txt", dict_str).expect("failed to write");
+    fs::write(out_file, dict_str).expect("failed to write");
 }
 
 fn collect_derivations(original: &Vec<&str>) -> HashSet<String> {
     let mut derived = original.iter()
-        .filter(|s| !s.is_empty() && s.chars().filter(|c| *c == '$').count() < 2)
         .map(|s| (*s).to_owned())
         .collect::<HashSet<String>>();
     for line in original.iter() {
