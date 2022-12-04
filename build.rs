21cb9c0668f1ff735a329abc85241fcd1bc88824
Mark <mverleg.noreply@gmail.com>
2022-12-04 23:51:15 +0100

    Try to exclude special symbols from dict

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use ::std::fs::read_to_string;
+use ::std::fs;
 use ::std::collections::HashSet;
 
 include!("src/common/dict_derive.rs");
@@ -11,19 +11,35 @@ fn main() {
 fn derived_dict_entries() {
     println!("cargo:rerun-if-changed=src/common/dict_derive.rs");
     println!("cargo:rerun-if-changed=dictionary.txt");
-    let base_dict = read_to_string("./dictionary.txt").unwrap();
-    let lines = base_dict.lines().collect::<Vec<_>>();
-    let mut known = lines.iter()
+    let base_dict = fs::read_to_string("./dictionary.txt").unwrap();
+    let original = base_dict.lines().collect::<Vec<_>>();
+    let derived = collect_derivations(&original);
+    println!("would count: {} -> {}", original.len(), derived.len());
+    let dict_str = sorted_join(derived);
+    fs::write("dictionary_extended.txt", dict_str).expect("failed to write");
+}
+
+fn collect_derivations(original: &Vec<&str>) -> HashSet<String> {
+    let mut derived = original.iter()
+        .filter(|s| !s.is_empty() && s.chars().filter(|c| *c == '$').count() < 2)
         .map(|s| (*s).to_owned())
         .collect::<HashSet<String>>();
-    for line in lines.iter() {
+    for line in original.iter() {
         for deriv in derivations((*line).to_owned()) {
-            if known.contains(&deriv.text) {
-                //TODO @mark:
-            } else {
-                known.insert(deriv.text.clone());
-            }
+            derived.insert(deriv.text.clone());
         }
     }
-    println!("would count: {} -> {}", lines.len(), known.len());
+    derived
+}
+
+fn sorted_join(derived: HashSet<String>) -> String {
+    let mut derived = derived.into_iter()
+        .collect::<Vec<_>>();
+    derived.sort();
+    let mut dict_str = String::with_capacity(derived.len() * 16);
+    for deriv in derived {
+        dict_str.push_str(&deriv);
+        dict_str.push('\n')
+    }
+    dict_str
 }
