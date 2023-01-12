f10b6d512efef896cf18fc1c47d9dfcec1df661d
Mark <mark@local>
2023-01-12 18:20:45 +0100

    Unit test for TinyMap non-duplicate entries

--- src/common/tiny_map.rs
+++ src/common/tiny_map.rs
@@ -21,6 +21,14 @@ impl <K: Default, V: Default> TinyMap<K, V> {
 }
 
 impl <K: Default + Eq + Hash, V: Default> TinyMap<K, V> {
+
+    pub fn len(&self) -> usize {
+        match self {
+            TinyMap::Small(vec) => vec.len(),
+            TinyMap::Big(map) => map.len(),
+        }
+    }
+
     pub fn get(&self, key: K) -> Option<&V> {
         match self {
             TinyMap::Small(vec) => {
@@ -52,5 +60,21 @@ impl <K: Default + Eq + Hash, V: Default> TinyMap<K, V> {
                 map.insert(key, value);
             },
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_unique() {
+        let mut map = TinyMap::new();
+        for i in 0..=20 {
+            map.insert(i, i * 10);
+            assert_eq!(map.get(i), Some(&(10 * i)));
+            assert_eq!(map.get(i + 1), None);
+            assert_eq!(map.len(), i + 1);
+        }
     }
 }
