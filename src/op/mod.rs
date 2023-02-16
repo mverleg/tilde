972fc16a7202fa85825844db9a8d08e69d1cb18a
Mark <mverleg.noreply@gmail.com>
2023-02-16 23:21:41 +0100

    Turn possible operations from enum types to values
    I want the operations to
    * Have all properties for a single operation in a single place, e.g.
      one file with arithmetic operations that has all the encodings,
      descriptions, metadata, execution code, etc. This is not the case if
      they are all variants of a single enum.
    * Have them easily enumerable e.g. for parsing lookup. This is not the
      case if each of them are types, they need instances. Especially
      doesn't work with structs, which aren't enumerable like enums.
    
    Making each option a struct instance achieves this. However:
    * It actually carries all the data around, as opposed to having it
      statically associated, as it would if it were a type.
    * It doesn't enforce that options are singletons, i.e. one Text option
      is the same as any other.

--- src/op/mod.rs
+++ src/op/mod.rs
@@ -1,6 +1,36 @@
 pub use ::std::slice;
 use ::std::vec;
-
-pub use self::ops::Op;
+use ::std::hash;
+use ::std::hash::Hasher;
 
 mod ops;
+
+pub const fn op_options() -> &'static [Op] {
+    todo!()
+}
+
+#[derive(Debug, Clone)]
+pub struct Op {
+    id: u16,
+}
+
+impl PartialEq for Op {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+impl Eq for Op {}
+
+impl hash::Hash for Op {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        state.write_u16(self.id)
+    }
+}
+
+//TODO @mark tests:
+// incrementing ids
+// unique long and short names
+// correct tokens in long and short names
+// parsing and rendering are eachothers opposite
+// lookup by id? for dense encoding?
+
