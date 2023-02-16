7b141142180d19e93d893b1aca11a594bc524ce9
Mark <mark@local>
2023-02-16 06:58:14 +0100

    Remove incorrect restriction that nr of bytes if even in b64 decode

--- src/common/base64.rs
+++ src/common/base64.rs
@@ -31,8 +31,7 @@ pub fn b64_decode(base64_source: &str) -> TildeRes<Vec<Letter>> {
     let Ok(src_bytes) = URL_SAFE_NO_PAD.decode(base64_source) else {
         return Err("base64 encoding not valid, alphabet should be A-Za-z0-9-_ without padding".to_string());
     };
-    debug_assert!(src_bytes.len() % 2 == 0);
-    let mut letters = Vec::with_capacity(src_bytes.len() / 2);
+    let mut letters = Vec::with_capacity(src_bytes.len() * 2);
     for byte in src_bytes {
         letters.push(Letter::from_nr(byte / 16));
         letters.push(Letter::from_nr(byte % 16));
@@ -66,7 +65,7 @@ mod tests {
             let enc = b64_encode(input).unwrap();
             let dec = b64_decode(&enc).unwrap();
             assert_eq!(*input, dec);
-            assert!(enc.len() < dec.len());
+            assert!(enc.len() <= dec.len());
         }
     }
 }
