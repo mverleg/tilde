24c2125950d91a2e18b0f7141cc4a49048d77edd
Mark <mverleg.noreply@gmail.com>
2023-02-17 23:06:09 +0100

    Make a trait for operations, plan to use structs after all

--- src/op/mod.rs
+++ src/op/mod.rs
@@ -1,3 +1,19 @@
-pub use self::ops::Op;
+use ::std::fmt::Debug;
 
-mod ops;
+trait Op: Debug {
+    fn id(&self) -> u8;
+
+    fn name(&self) -> &'static str;
+
+    fn description(&self) -> &'static str;
+
+    fn long_code(&self) -> Option<()>;
+
+    fn golf_code(&self) -> Option<()>;
+
+    //TODO @mark: evaluation methods
+}
+
+//TODO @mark: long and gold not both empty
+//TODO @mark: id unique and sequential
+//TODO @mark: name unique and identifier-safe
