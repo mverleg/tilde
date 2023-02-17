9200161a5352df54f49086ca457f735f571660f8
Mark <mverleg.noreply@gmail.com>
2023-02-17 15:04:05 +0100

    Revert "Turn possible operations from enum types to values"
    This reverts commit 972fc16a7202fa85825844db9a8d08e69d1cb18a.

--- src/op/mod.rs
+++ src/op/mod.rs
@@ -1,36 +1,6 @@
 pub use ::std::slice;
 use ::std::vec;
-use ::std::hash;
-use ::std::hash::Hasher;
+
+pub use self::ops::Op;
 
 mod ops;
-
-pub const fn op_options() -> &'static [Op] {
-    todo!()
-}
-
-#[derive(Debug, Clone)]
-pub struct Op {
-    id: u16,
-}
-
-impl PartialEq for Op {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
-}
-impl Eq for Op {}
-
-impl hash::Hash for Op {
-    fn hash<H: hash::Hasher>(&self, state: &mut H) {
-        state.write_u16(self.id)
-    }
-}
-
-//TODO @mark tests:
-// incrementing ids
-// unique long and short names
-// correct tokens in long and short names
-// parsing and rendering are eachothers opposite
-// lookup by id? for dense encoding?
-
