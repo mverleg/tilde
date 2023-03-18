835e57f0e96775d073b72495f602e9e02b187879
Mark <mverleg.noreply@gmail.com>
2023-03-18 22:50:33 +0100

    Test presedence of func captures

--- examples/simple_closure.rs
+++
@@ -1,7 +0,0 @@
-use ::tilde::tilde_strs;
-
-fn main() {
-    let prog = "arg 1 plus plus apply";
-    let res = tilde_strs(prog, "1\n2\n3\n4\n5").unwrap();
-    assert_eq!(res, "20");
-}
---
+++ examples/simple_closures.rs
@@ -0,0 +1,12 @@
+use ::tilde::tilde_strs;
+
+fn main() {
+    // map and reduce array
+    let prog = "arg 1 plus plus apply";
+    let res = tilde_strs(prog, "1\n2\n3\n4\n5").unwrap();
+    assert_eq!(res, "20");
+
+    // compare x-3 and 3-x (scalar apply)
+    assert_eq!(tilde_strs("arg 3 sub 5 apply", ""), Some("2"));
+    assert_eq!(tilde_strs("3 arg sub 5 apply", ""), Some("-2"));
+}
