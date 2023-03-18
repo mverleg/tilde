1f35c9ab478d13d52160f54e59efd385e8ab7917
Mark <mverleg.noreply@gmail.com>
2023-03-18 16:07:22 +0100

    Add another example

---
+++ examples/isbn_check.rs
@@ -0,0 +1,13 @@
+
+// https://code.golf/isbn
+
+use ::tilde::tilde_strs;
+
+fn main() {
+    let prog = "lookup base-words unique count";
+    assert_eq!(tilde_strs(prog, "5-3614111-3-"), Ok("5-3614111-3-2".to_owned()));
+    assert_eq!(tilde_strs(prog, "5-80635-550-"), Ok("5-80635-550-0".to_owned()));
+    assert_eq!(tilde_strs(prog, "4-57803-516-"), Ok("4-57803-516-6".to_owned()));
+    assert_eq!(tilde_strs(prog, "5-36723-465-"), Ok("5-36723-465-8".to_owned()));
+    assert_eq!(tilde_strs(prog, "0-821-99799-"), Ok("0-821-99799-8".to_owned()));
+}
