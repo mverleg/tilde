272fbad39dc58d9880357fd53b354008ee370e46
Mark <mverleg.noreply@gmail.com>
2023-03-18 16:57:16 +0100

    Add new function binary methods

--- src/exec/executor.rs
+++ src/exec/executor.rs
@@ -48,17 +48,31 @@ pub trait BinaryExecutor: OpTyp {
 
     fn exec_na(&self, deep: Nr, top: Array) -> Values;
 
+    fn exec_nf(&self, deep: Nr, top: Func) -> Values;
+
     fn exec_tn(&self, deep: Text, top: Nr) -> Values;
 
     fn exec_tt(&self, deep: Text, top: Text) -> Values;
 
     fn exec_ta(&self, deep: Text, top: Array) -> Values;
 
+    fn exec_tf(&self, deep: Text, top: Func) -> Values;
+
     fn exec_an(&self, deep: Array, top: Nr) -> Values;
 
     fn exec_at(&self, deep: Array, top: Text) -> Values;
 
     fn exec_aa(&self, deep: Array, top: Array) -> Values;
+
+    fn exec_af(&self, deep: Array, top: Func) -> Values;
+
+    fn exec_fn(&self, deep: Func, top: Nr) -> Values;
+
+    fn exec_ft(&self, deep: Func, top: Text) -> Values;
+
+    fn exec_fa(&self, deep: Func, top: Array) -> Values;
+
+    fn exec_ff(&self, deep: Func, top: Func) -> Values;
 
     /// Fallback for if there is only 1 value on the stack and it is a number
     fn exec_single_n(&self, single: Nr) -> Values;
@@ -68,6 +82,9 @@ pub trait BinaryExecutor: OpTyp {
 
     /// Fallback for if there is only 1 value on the stack and it is a array
     fn exec_single_a(&self, single: Array) -> Values;
+
+    /// Fallback for if there is only 1 value on the stack and it is a function
+    fn exec_single_f(&self, single: Func) -> Values;
 
     /// Fallback for if the stack is empty
     fn exec_empty(&self) -> Values;
