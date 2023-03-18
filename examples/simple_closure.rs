eb91b0921dd820a1144c4f5da2edd15a4f5307c8
Mark <mverleg.noreply@gmail.com>
2023-03-18 16:33:20 +0100

    Implement map, but that is maybe premature

--- examples/simple_closure.rs
+++ examples/simple_closure.rs
@@ -1,7 +1,7 @@
 use ::tilde::tilde_strs;
 
 fn main() {
-    let prog = "arg 1 plus map sum";
+    let prog = "arg 1 plus apply sum";
     let res = tilde_strs(prog, "1\n2\n3\n4\n5").unwrap();
     assert_eq!(res, "20");
 }
--- src/op/func.rs
+++ src/op/func.rs
@@ -3,7 +3,7 @@ use ::std::borrow::Cow;
 
 use crate::{Array, Nr, Text, Values, values};
 use crate::compile::GolfWord;
-use crate::exec::{Executor, NullaryExecutor, UnaryExecutor};
+use crate::exec::{BinaryExecutor, Executor, NullaryExecutor, UnaryExecutor};
 use crate::op::{Op, OpTyp};
 use crate::op::collection::Split;
 
@@ -42,6 +42,93 @@ impl OpTyp for Arg {
 impl NullaryExecutor for Arg {
 
     fn exec(&self) -> Values {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Map;
+
+impl Map {
+    pub fn new() -> Op {
+        Op::of(Map)
+    }
+}
+
+impl OpTyp for Map {
+
+    fn description(&self) -> &'static str {
+        "Starts a new closure, representing its argument\nmost operations applied to arg are added to the closure instead of executed. A few operations will execute the closure on some input, possibly repeatedly."
+    }
+
+    fn long_code(&self) -> Cow<'static, str> {
+        Cow::Borrowed("map")
+    }
+
+    fn golf_code(&self) -> Option<GolfWord> {
+        None
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_executor(&self) -> Executor {
+        Executor::Binary(self)
+    }
+}
+
+impl BinaryExecutor for Map {
+
+    fn exec_nn(&self, deep: Nr, top: Nr) -> Values {
+        todo!()
+    }
+
+    fn exec_nt(&self, deep: Nr, top: Text) -> Values {
+        todo!()
+    }
+
+    fn exec_na(&self, deep: Nr, top: Array) -> Values {
+        todo!()
+    }
+
+    fn exec_tn(&self, deep: Text, top: Nr) -> Values {
+        todo!()
+    }
+
+    fn exec_tt(&self, deep: Text, top: Text) -> Values {
+        todo!()
+    }
+
+    fn exec_ta(&self, deep: Text, top: Array) -> Values {
+        todo!()
+    }
+
+    fn exec_an(&self, deep: Array, top: Nr) -> Values {
+        todo!()
+    }
+
+    fn exec_at(&self, deep: Array, top: Text) -> Values {
+        todo!()
+    }
+
+    fn exec_aa(&self, deep: Array, top: Array) -> Values {
+        todo!()
+    }
+
+    fn exec_single_n(&self, single: Nr) -> Values {
+        todo!()
+    }
+
+    fn exec_single_t(&self, single: Text) -> Values {
+        todo!()
+    }
+
+    fn exec_single_a(&self, single: Array) -> Values {
+        todo!()
+    }
+
+    fn exec_empty(&self) -> Values {
         todo!()
     }
 }
