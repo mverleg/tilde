c0ffeeadb41a040a1b5a23de48c06397a0ab702a
Mark <mverleg.noreply@gmail.com>
2023-03-26 14:48:39 +0200

    Fixes for multi-arg example code

--- examples/closures_multi_arg.rs
+++ examples/closures_multi_arg.rs
@@ -2,8 +2,8 @@ use ::tilde::tilde_strs;
 
 fn main() {
     // pythagoras, implicit apply
-    let prog = "arg dup mul arg dup mul sqrt";
-    let res = tilde_strs(prog, "4 3").unwrap();
+    let prog = "arg dup mul arg dup mul plus sqrt";
+    let res = tilde_strs(prog, "4\n3").unwrap();
     assert_eq!(res, "5");
 
     // capture order
