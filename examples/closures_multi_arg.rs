c0ffee03844fb3aa018dc13b5370b9f76990742a
Mark <mverleg.noreply@gmail.com>
2023-03-28 23:29:48 +0200

    Add some todos

--- examples/closures_multi_arg.rs
+++ examples/closures_multi_arg.rs
@@ -2,7 +2,10 @@ use ::tilde::tilde_strs;
 
 fn main() {
     // pythagoras, implicit apply
-    let prog = "last arg dup mul arg dup mul plus sqrt";
+    //TODO @mark: there is a bug here, which may be hard to solve: dup has two return values, but
+    //TODO @mark: Tilde does not know that while building the closure, so `arg dup` is one value,
+    //TODO @mark: making the next binary op (`mul`) take the closure and the prev value
+    let prog = "flatten arg dup mul arg dup mul plus sqrt";
     let res = tilde_strs(prog, "4\n3").unwrap();
     assert_eq!(res, "5");
 
